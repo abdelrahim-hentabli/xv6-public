23234a0a413de1a0f4ab59d17d55d4480a98494d
Samuel Tapia <stapi010@ucr.edu>
2021-05-30 10:19:59 -0700

    added thread.c library func calls

--- thread.c
+++ thread.c
@@ -9,33 +9,41 @@
 struct lock_t lock;
 
 int
-thread_create(void*(*start_routine)(void*), void *arg)
+thread_create(void *(*start_routine)(void*), void *arg)
 {
     // uses clone to create threads
-    
-    return
+    void *stack = malloc(1024);
+    int tid = clone(stack, 1024);
+    if(!tid)
+        (*start_routine)(arg);
+    return tid;
+}
+
+//initialize lock to 0. Reference from spinlock.c
+int
+lock_init(struct lock_t * lt)
+{
+    lt->locked = 0;
 }
 
 int
-lock_init(lock_t *)
+lock_acquire(lock_t *lt)
 {
-    
+    // The xchg is atomic. Reference from spinlock.c
+    while(xchg(&lt->locked, 1) != 0)
+        ;    
 }
 
 int
-lock_acquire(lock_t *)
-{
-    
-}
-
-int
-lock_release(lock_t*)
-{
-    
+lock_release(lock_t* lt)
+{  
+    //releases lock
+    xchg(&lt->locked, 0);
 }
 
 void
-thread_join(lock_t*)
+thread_join(lock_t* lt)
 {
-
+    //reap children
+    wait();
 }
